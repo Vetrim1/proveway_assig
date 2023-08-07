5c09a3dd57be421db6bd47eb0894fcbaaed7b4ba
Vetrim1 <85014269+Vetrim1@users.noreply.github.com>
2023-08-07 08:47:18 +0530

    3 files modified

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # proveway_assig
 
+first we create line within text & make it after& before effect
+
+<h2 class="head"><span>Bundle & Save</span></h2>
+
+next create a container for box model
+
+In this box contain radio button.
+using that buttton we achiev our result
+Using onclick event we send a parameter like table id for table shown, box id for background change,p1 for get a price details.
+
+<!-- onclick="triger('tt1','b1','p1')" -->
+
+Using onclick event we send a parameter like id,a,b
+table id for table shown, box id for background change,p1 for get a price details
+
+<!-- Object.values(table).map((item, index) => {
+table[index].style.display = 'none';
+}); -->
+
+for onclick event we show the table block
+
+<!-- let sel = document.getElementById(id);
+sel.style.display = 'block'; -->
+
+for onclick event we change the background
+
+<!-- let sel1 = document.getElementById(a);
+sel1.style.cssText = 'border: 1px solid #007F61;background-color: #F4FBF9;'; -->
+
+get onclick price detail in total
+
+<!-- let price = document.getElementById(b).innerHTML;
+document.querySelector('.total').innerHTML = `Total : ${price}`;
+}; -->
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-bw1qq4)
--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// window.onload = () => {
-//     const node = document.querySelector('.tableclone');
-//     const clone = node.cloneNode(true);
-//     document.getElementById('tt2').appendChild(clone);
-
-//     document.getElementById('tt3').appendChild(
-//         document.querySelector('.tableclone').cloneNode(true)
-//     );
-// }
-
 let triger = (id, a, b) => {
   var table = document.querySelectorAll('.table');
   var box = document.querySelectorAll('.box');
+
+  // Using onclick event we send a parameter like id,a,b
+  // table id for table shown, box id for background change,p1 for get a price details
 
   Object.values(table).map((item, index) => {
     table[index].style.display = 'none';
@@ -22,19 +15,62 @@ let triger = (id, a, b) => {
         'border: 1px solid black;background-color: white;';
     }
   });
-
+  //for onclick event we show the table block
   let sel = document.getElementById(id);
   sel.style.display = 'block';
 
+  //for onclick event we change the background
   let sel1 = document.getElementById(a);
   sel1.style.cssText = 'border: 1px solid #007F61;background-color: #F4FBF9;';
 
+  //get onclick price detail in total
   let price = document.getElementById(b).innerHTML;
   document.querySelector('.total').innerHTML = `Total : ${price}`;
 };
+
+//It can be use for cannot select any pair then the add card button will alert
 let add = () => {
   let total = document.querySelector('.total').innerHTML;
   total !== 'DKK 0.00'
     ? alert('cart added successfuly')
     : alert('plz select first');
 };
+
+//nothing////
+/////////////////////////////////////////////////
+// window.onload = () => {
+//     const node = document.querySelector('.tableclone');
+//     const clone = node.cloneNode(true);
+//     document.getElementById('tt2').appendChild(clone);
+
+//     document.getElementById('tt3').appendChild(
+//         document.querySelector('.tableclone').cloneNode(true)
+//     );
+
+//  window.onload=()=>{
+//     //let tab1=[[ , 'size','colour'],['#1',]]
+//     var table = document.createElement('table');
+//     table.setAttribute("class", "tableclone");
+//     table.style.emptyCells="hide";
+//     document.getElementById('tt2').appendChild(table);
+//     var header = table.createTHead();
+//     var row = header.insertRow();
+//     var th = document.createElement('th')
+//     th.innerHTML='';
+//     var th1 = document.createElement('th')
+//     th1.innerHTML='size';
+//     var th2 = document.createElement('th')
+//     th2.innerHTML='colour';
+//     row.appendChild(th)
+//     row.appendChild(th1)
+//     row.appendChild(th2)
+
+//     var tbody = table.createTBody();
+//     tbody.style.backgroundColor="white;"
+//     var brow = tbody.insertRow();
+//     var cell = brow.insertCell(0)
+
+//     var select = document.createElement("select");
+//     //row.appendChild(select)
+//  }
+// }
